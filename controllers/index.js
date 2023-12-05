a79c046f55435d7a2aaf8c93aec014ce3d129a33
astand02 <135463433+astand02@users.noreply.github.com>
2023-12-05 08:51:06 -0600

    added routes

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const postRoutes = require('./postRoutes'); 
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/post', postRoutes); 
+
+module.exports = router;
