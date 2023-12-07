c9cf91343ab6ff00a57f9cbd41a0ae316c529f7c
astand02 <135463433+astand02@users.noreply.github.com>
2023-12-07 10:08:36 -0600

    fixed errors

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -4,13 +4,13 @@ const { Post } = require('../models');
 //Post
 router.post('/post', async (req, res) => {
     try {
-      const { title, content, author } = req.body;
+      const { title, content, userId } = req.body;
 
       if (!content) {
         return res.status(400).json({ message: 'Text is required to create this post!' });
       }
 
-      const post = await Post.create({ title, content, author });
+      const post = await Post.create({ title, content, userId });
 
       console.log('New post created:', post.toJSON()); 
 
@@ -23,27 +23,6 @@ res.status(201).json(postObject);
       res.status(500).json({ message: 'Internal Server Error' });
     }
   });
-// Edit post
-router.put('/:id', (req, res) => {
-  Post.update(
-    {
-      title: req.body.title,
-      content: req.body.content
-    },
-    {
-      where: {
-        post_id: req.params.post_id,
-      },
-    }
-  )
-    .then((updatedPost) => {
-      res.json(updatedPost);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
-});
 
 // Delete post
 router.delete('/post/delete/:id', async (req, res) => {
