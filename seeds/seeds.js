878f4fd7ec2b763a1270a34de736b792e66d44fb
astand02 <135463433+astand02@users.noreply.github.com>
2023-12-06 22:53:09 -0600

    added files and code  to seeds

---
+++ seeds/seeds.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection.js');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const user = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      userId: user[Math.floor(Math.random() * user.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
